2a5407f5dc9940cea2f448fe25f095fbb0c293dc
Henrique Bisneto <bis_henrique@yahoo.com.br>
2024-04-13 23:19:57 -0300

    build: add dependency archive

---
+++ Downloads/internship-system/src/main/java/com/backend/internshipsystem/InternshipSystemApplication.java
@@ -0,0 +1,13 @@
+package com.backend.internshipsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipSystemApplication.class, args);
+	}
+
+}
